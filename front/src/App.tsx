53955b7139fb16f8898df37686f11167b142c4bc
HoloTWW <Ivanchenkov.business@yandex.ru>
2025-04-02 13:32:13 +0300

    prot: app

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Menu from './Menu/Menu';
+import SignIn from './Auth/SignIn';
+import SignUp from './Auth/SignUp';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ContactList from './Menu/ModalComponent/ContactList';
+import ChatList from './ChatList/ChatList';
+import Chat from './Chat/Chat';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<BrowserRouter>
+  <div className='vh-100 position-absolute'>
+    <div className='row h-100'>
+      <div style={{overflowY:"scroll", overflowX:"hidden"}} className='col-3 m-0 p-0 h-100'>
+        <Menu/>
+        <ChatList/>
+      </div>
+      <div className='col bg-secondary-subtle pt-2 position-relative h-100 overflow-hidden px-0' >
+        <Chat/>
+      </div>
     </div>
+  </div>
+  <Routes>
+    <Route path="/auth/sign-in" element={<SignIn/>}/>
+    <Route path="/auth/sign-up" element={<SignUp/>} />
+  </Routes>
+</BrowserRouter>
   );
 }
 
 export default App;
+
+// <div className="App bg-secondary-subtle m-5 p-3">
+        // {/* <Card/> */}
+        // </div>
