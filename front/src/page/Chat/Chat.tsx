15c6246c8325356050494e0a331cc711693fcb40
HoloTWW <Ivanchenkov.business@yandex.ru>
2025-04-09 01:18:03 +0300

    feat: adaptive center chat

--- front/src/page/Chat/Chat.tsx
+++ front/src/page/Chat/Chat.tsx
@@ -1,10 +1,10 @@
+import { CSSProperties, useEffect, useState } from "react"
 
 // me
 const Sender = () =>{
     return(<>
-<div className=" d-flex justify-content-start mb-2">
-    <img  height={"60px"} className="rounded d-flex align-self-" src="/default/avatar.jpeg" alt={"avatar.jpg"}/>
-    <div style={{maxWidth:"35%"}} className=" bg-primary-subtle p-2 ms-2 rounded ">
+<div className=" d-flex justify-content-end mb-2">
+    <div style={{maxWidth:"60%"}} className=" bg-primary-subtle p-2 mx-2 rounded ">
         <div className="d-flex justify-content-between">
             <span className=" fw-bold">senderName</span>
             <span>
@@ -16,7 +16,8 @@ const Sender = () =>{
         <div>
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
         </div>
-    </div>   
+    </div>
+    <img  height={"60px"} className="rounded d-flex align-self-" src="/default/avatar.jpeg" alt={"avatar.jpg"}/>   
 </div>
     </>)
 }
@@ -24,9 +25,9 @@ const Sender = () =>{
 // other
 const Recipient = () =>{
     return(<>
-<div className=" d-flex justify-content-start mb-2">
+<div className=" d-flex justify-content-start mb-2 ">
     <img  height={"60px"} className="rounded d-flex align-self-" src="/default/avatar.jpeg" alt={"avatar.jpg"}/>
-    <div style={{maxWidth:"35%"}} className=" bg-secondary p-2 ms-2 rounded ">
+    <div style={{maxWidth:"60%"}} className=" bg-secondary p-2 mx-2 rounded ">
         <div className="d-flex justify-content-between">
             <span className=" fw-bold">recipientName</span>
             <span>
@@ -45,23 +46,36 @@ const Recipient = () =>{
 
 
 const Chat = () =>{
-    return(<div className="d-flex flex-column h-100">
-    <div className=" overflow-x-hidden px-5 flex-grow-1 ">
-    <Sender/>
-    <Recipient/>
-    <Sender/>
-    <Sender/>
-    <Sender/>
-    <Recipient/>
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    useEffect(() => {
+        const handleResize = () => {
+          setWindowWidth(window.innerWidth);
+        };
+    
+        window.addEventListener('resize', handleResize);
+    
+        // Очистка при размонтировании компонента
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+
+      const st = {
+        margin: windowWidth <= 1140 ? '0' : '0 10% 0 10%',  // Адаптивный отступ
+        // Другие стили
+      };
+    return(
+<div style={st} className="d-flex flex-column h-100">
+    <div className=" overflow-x-hidden px-5 flex-grow-1">
     <Recipient/>
     <Sender/>
         {/* это примеры сообщений  */}
     </div>
-    <div className="px-3 py-2 w-50 input-group">
-        <input className=" form-control"/>
+    <div className=" px-3 py-2 input-group">
+        <textarea rows={1} className=" form-control"/>
         <button className="btn btn-outline-primary">Send</button>
     </div>
-    </div>)
-}
+</div>
+)};
 
 export default Chat;
